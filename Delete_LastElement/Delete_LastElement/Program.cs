f3d4117b9606c2f76e8f32c863ba239820d5da54
vrb9999 <vbiradar9999@gmail.com>
2022-05-11 00:43:41 +0530

    UC-6 Day-14 Delete last element

---
+++ Delete_LastElement/Delete_LastElement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delete_LastElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.PopLast();
+            list.Display();
+        }
+    }
+}
