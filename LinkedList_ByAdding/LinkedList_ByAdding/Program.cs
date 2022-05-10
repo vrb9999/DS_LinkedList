9b5636027a5b782235db0fc84e1acd567768731f
vrb9999 <vbiradar9999@gmail.com>
2022-05-11 00:39:18 +0530

    UC-1 Day-14 Adding elements

---
+++ LinkedList_ByAdding/LinkedList_ByAdding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList_ByAdding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
