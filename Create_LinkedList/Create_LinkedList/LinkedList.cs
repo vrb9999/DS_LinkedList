f0b54b5ed522cd4d64532d7a7764015d411c1902
vrb9999 <vbiradar9999@gmail.com>
2022-05-11 00:36:28 +0530

    UC-1 Day-14 Create linked list

---
+++ Create_LinkedList/Create_LinkedList/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Create_LinkedList
+{
+    class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data + "\n");
+            }
+        }
+    }
+}
