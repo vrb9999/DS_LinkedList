75ed0778369ca886d55c7425681a280549cc3442
vrb9999 <vbiradar9999@gmail.com>
2022-05-11 00:41:30 +0530

    UC-4 Day-14 Inserting element at particular position

---
+++ InsertAtPosition/InsertAtPosition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InsertAtPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(70);
+            list.AddAfter(0, 30);
+            list.Display();
+        }
+    }
+}
