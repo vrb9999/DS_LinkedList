7b7415570c675f15b401ed8c11a309dd3c911a30
vrb9999 <vbiradar9999@gmail.com>
2022-05-11 00:40:08 +0530

    UC-3 Day-14 Appending elements

---
+++ LinkedList_ByAppending/LinkedList_ByAppending/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList_ByAppending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Append(30);
+            list.Append(70);
+            list.Display();
+        }
+    }
+}
