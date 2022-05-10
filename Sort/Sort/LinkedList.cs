97fb45dce85b1fbd881d0eeea65ddacdd85725a0
vrb9999 <vbiradar9999@gmail.com>
2022-05-11 00:48:37 +0530

    UC-10 Day-14 Sorting elements in Ascending order

---
+++ Sort/Sort/LinkedList.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sort
+{
+    class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                node.next = head;
+                this.head = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        public void Find(int value)
+        {
+            Node temp = head;
+            int count = 0;
+
+            while (temp.data != value)
+            {
+                count++;
+                temp = temp.next;
+            }
+            Console.WriteLine(value + " index value is " + count);
+        }
+        public int Length()
+        {
+            Node temp;
+            int Count = 0;
+            temp = this.head;
+            while (temp != null)
+            {
+                Count++;
+                temp = temp.next;
+            }
+            return Count;
+        }
+
+        public void AddAfter(int Ploc, int data)
+        {
+            Node temp;
+            Node p;
+            int loc = Ploc, i = 1;
+            int leng = this.Length();
+            if (loc > leng)
+            {
+                Console.WriteLine("Invalid Location");
+                Console.WriteLine("Current list having ", leng);
+            }
+            else
+            {
+                p = this.head;
+                while (i < loc)
+                {
+                    p = p.next;
+                    i++;
+                }
+                Node node = new Node(data);
+                temp = node;
+                temp.next = p.next;
+                Console.WriteLine("{0} inserted into after {1} linked list", node.data, p.data);
+                p.next = temp;
+            }
+        }
+        public void SortAscending()
+        {
+            Node temp = this.head;
+            Node firstVal = null;
+            int val = 0;
+            while (temp != null)
+            {
+                firstVal = temp.next;
+                while (firstVal != null)
+                {
+                    if (temp.data > firstVal.data)
+                    {
+                        val = firstVal.data;
+                        firstVal.data = temp.data;
+                        temp.data = val;
+                    }
+                    firstVal = firstVal.next;
+                }
+                temp = temp.next;
+            }
+            Console.WriteLine("Sorted Linked List :");
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.Write(temp.data + "\n");
+            }
+        }
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Add(40);
+            list.Display();
+            list.SortAscending();
+            list.Display();
+        }
+    }
+}
